bac4eb73c5ac0cd068d8322da3ae31698801b328
1587 <zjfscu@gmail.com>
2022-06-12 14:22:21 +0800

    Create tmp.mk

---
+++ tmp.mk
@@ -0,0 +1,371 @@
+## 7.	真题7（老师能否让学生带电子设备）
+2021. 10/16
+### 7.1	part1（1~2）(45+45)
+Banquet
+
+### 7.2	part2（3 ~ 4）(30+30) 6~8个句子
+
+办公室，2人，很多书。左边一个老妇女坐在凳子上桌子前打着电话。右边一个男的蹲在地上从小书架上拿一本书，墙壁上有很多便签贴纸
+
+![image](https://user-images.githubusercontent.com/2299635/171655770-077bc146-5fb8-4c76-94c8-4a816d654e30.png)
+
+My answer:
+
+```
+This picture is taken in an office. there are two people and many books in this picture.
+On the left side, there is an old woman who is sitting on the chair in front of the desk, 
+she is calling on the phone.
+On the right side, there is a man who is squatting [skwɑːt] to get the book from the shelf 
+In front of them, there are many notepapers on the wall. 
+```
+
+图2 在一个食品店里，两个人，还有很多食物-（我口语说是desert-甜点，也可以说面包）。左边男服务员正在打字，中间的女的是顾客，正在看她的钱包（从钱包掏钱）。右边橱窗有很多食物（我说dessert）
+
+![image](https://user-images.githubusercontent.com/2299635/171655810-48f38cc4-39f3-42aa-a5e2-a26c500c0130.png)
+
+My answer:
+
+```
+This picture is taken at the food store, there are two people in this picture.
+On the left side, there is a man who is the shop assistant/store's staff 
+and typing on the keyboard.
+In the middle of this picture, there is a woman who is taking money from her wallet.
+On the right side, there are many food on the shelf.
+```
+
+### 7.3	part3（5~7）(15+15+30)
+
+你上次住hotel是什么时候？在哪里？
+What is the one feature a new hotel should have？Why（不准确，应该是问新酒店应该具备一个什么特性特点）
+你觉得你所在的地方需要更多新hotels吗？Why or why not.
+
+My answer:
+
+```
+I stayed in the hotel last month in NewYork because of the business trip.
+
+I think the new hotel should provide more fitness equipment. Many people like fitness, fitness make us healthier.
+
+No. I live in Nanjing, There are too many hotels in the city. I think it is enough. 
+We need more parks, more bookstores, more schools and more museums.
+```
+
+### 7.4	part4（8~10）(15+15+30) 题目是听的
+ 
+Conference在什么时间什么地方举行？（有几个新词注意准备的时候要注意好）
+我需要自己准备午餐吗？
+我想参加关于manufacturing的会议，跟我说一下有哪些.
+
+My answer:
+```
+ 
+No. don’t warry about. Conference will provide the lunch at 12:30 by XX.
+
+OK, let me see, according to the schedule, there are two sessions about manufacturing. 
+One is at xx:xx named xxxx  in xxxx. 
+The other is. at xx:xx named xxxx  in xxxx.
+Please keep it in mind.
+```
+
+### 7.5	part5（11）(60)
+ 
+老师是否应该禁止学生带电子 设备 手机 电脑到教室？为什么？
+
+My answer:
+
+```
+Yes, Of course, teachers should forbid that. The reasons are as below.
+Firstly, Students should focus on study and learn more knowledge. Listen to the teacher carefully in class.
+Secondly, It is easy for students to play games with electronic devices, which wastes more time for them.
+Thirdly, electronic device is harmful to the students’ eyes. Many students will have poor eyesight.
+finally, There is a lot of bad knowledge on the Internet, students have no ability to identify it.
+The above reasons are why the electronic device should be forbidden.
+```
+
+
+
+
+
+## 6.	真题6(未来一代为什么比现在一代更健康)
+### 6.1	part1（1~2）(45+45)
+
+Reading：
+
+```
+(1) If you are looking for |something fun to do |this weekend, 
+come to Greenvale mountain resort [rɪˈzɔːt], 
+our resort offers exciting activities, relaxing rules, 
+and delicious food /that will delight /the whole family. 
+Located [ləʊˈkeɪtɪd] only two hours from downtown, 
+Greenvale mountain resort| is a perfect [ˈpɜːfɪkt] place for you to enjoy a family vacation [veɪˈkeɪʃn;. 
+Call us today!
+```
+
+```
+(2) I’m pleased to introduce Jackson financial’s newest [nju:ɪst] employee. 
+Ms xxx Hodge starting next week Ms Hodge will be in charge of research in the accounting [əˈkaʊntɪŋ] department, 
+she comes here with extensive [ɪkˈstensɪv] experience in marketing, 
+financial, economic [ˌiːkəˈnɒmɪk; analysis [əˈnæləsɪs] , 
+please join me in welcoming [ˈwelkəmɪŋ] Mrs Holmes To Jackson financial. [faɪˈnænʃəl].
+```
+
+### 6.2	part2（3 ~ 4）(30+30) 6~8个句子
+
+在一家衣服店，一个女的拿着衣服在身上看，一个女的看着她，后面是一个男的
+(图片发生在衣服店，左边有个穿着蓝色的T-shirt在试着白色的衣服，右边有个女的，可能是他妈妈，在帮忙看着衣服是否合适，最右边有一对金属物品在陈列着。)
+一家商店，三人，左后方一个白色T-shirt男，中间蓝色衣服女正在尝试一件白色衣服，右边是一个黑人妇女，最左边有很多商品（说金属物品应该也可以）
+
+![image](https://user-images.githubusercontent.com/2299635/171654973-5e9bff4c-01bd-4b73-a9c0-5f374b55e30a.png)
+
+My answer:
+
+```
+The picture is taken in a clothing store. 
+On the left side, one woman with a blue T-shirt is trying on white clothes. Another woman is looking at her. There is a man behind them. 
+On the right side, we can see a pair of metal [ˈmetəl] artworks and  there are many goods on the shelves
+```
+
+
+两人-1女1男，三辆car，一辆自行车。女的蹲地上做着什么（开锁吧），白色衬衣男正准备打开汽车门。路的另外一边有两辆车，还有很多树
+
+![image](https://user-images.githubusercontent.com/2299635/171654890-ca978991-b859-4328-87f8-2705526ebd75.png)
+
+My answer:
+
+```
+The picture is taken on the street. There are 2 people in this picture.
+On the right side, a man with a blue shirt is opening the door of the car. 
+On the left side, a woman is squatting/crouching on the side of a bicycle. Looks like she's locking her bike.
+In the background of the picture, we can see another 2 cars and many trees.
+Behind the trees, there is a car parking on the road and a man is going toward to the car.
+```
+
+### 6.3	part3（5~7）(15+15+30)
+1.	家乡呆了多长时间？ Where you lived before？How long did you lived there？
+2.	你现在呆的地方你最喜欢什么？Do you like to live in current place？Why？
+3.	如果你换了地方住，你最看重哪些点？为什么？If you want to change to another place，what is the most important？Why？
+
+My answer:
+
+```
+My hometown is a village in Anhui Province of China. I lived there for 18 years until I went to the college. 
+
+I live in Nanjing now, I like the museums best, the things inside the museums are beautiful. 
+
+If I moved to another place,
+The place must near the subway station. that will save a lot of time and money on transportation.
+It’s better to have the supermarkets and hospitals around the new place. that will be more convenient for my life.
+```
+
+
+### 6.4	part4（8~10）(15+15+30) 题目是听的
+儿童文学会议
+第一题：什么时候举办？
+第二题：有一本什么书，什么时候可以签名 I want to have the chance to sign a book written by XXX？Do I have the opportunity？
+第三题：对reading technology感兴趣，参加哪些
+
+My answer:
+
+```
+The event is Start at 8:30 on January 24.
+
+You are a luck guy. You can come here at 10:AM to join the conference 
+and you will get the book sign by XX Clark(前面一个极其生僻的人名)
+
+There are Two topics about reading technology. 
+One is at xx:xx in x月x日 given by M. xxxx;  
+another is at One is at xx:xx in x月x日th（如果有日）given by M. xxxx. 
+```
+
+
+### 6.5	part5（11）(60)
+现在的人和将来的人会比较健康，为什么？
+
+My answer:
+
+```
+According to my personal experience, I think people will be healthier in the future then they are now. 
+There are some reasons as follows:
+
+Firstly, Nowadays, People are paying more and more attention to their health.
+For example, my colleagues eat healthy foods, do more exercises, such as running and playing footballs.
+
+Secondly, medical technologies are improved. There are some new medical for more and more diseases /dɪˈziːzɪz/.
+
+Thirdly, People begin protecting the environment, and the environment of future will be better than now.
+
+To the reason above, I think people will be healthier in the future.
+```
+
+
+
+
+
+## 4.	真题4（offer是否要跟家里人交流）
+2021. 8.15/1.23
+### 4.1	part1（1~2）(45+45)
+### 4.2	part2（3 ~ 4）(30+30) 6~8个句子
+ 第三题：一张公园的照片，路上一个穿黑色裙子（连衣裙）的女（短发，背影）在走路，其他人在路边长条椅上坐着，最近的椅子上，一人在看报，一人在看手机。路边右侧是公园的绿草地，草地上有大树。远处背景有很多人。
+
+My answer:
+
+```
+I think this picture is taken in a park, there are many people in the picture,
+in the middle of the picture, there is a girl with black skirt walking around, 
+next to her, there are some other people sitting on the bench, 
+in the background of the picture, there are many trees and green grass,
+the environment is beautiful, it seems like a good place for people to take a rest and have a good time with friends.
+```
+
+第四题：一张草地农场照，绿色隔离网分成了两部分。左下半部分是三个人，两女一男。左上的女的站着，手里拿着相机，中间女的长发站着，右肩上背着红色的包。右边男的蹲着地上，隔着隔离网喂鸡。整个农场都是绿色草地，远处有房子。
+
+My answer:
+
+```
+The picture shows there are 2 girls and 1 boy in a farm. 
+The boy with short hair and a gray shirt is on the right side of the picture. He is feeding chickens. 
+On the left side, there is a girl with long hair standing and looking at the boy. 
+Another girl is taking photos for them by a smartphone. In the background of the picture,
+I can see more chickens and some buildings.
+I think they feel happy.
+```
+
+### 4.3	part3（5~7）(15+15+30)
+1. 你上次买电脑是什么时候，为什么买？
+2. 你觉得电脑在线上卖的多还是线下卖的多？
+3. 你觉得电脑最重要的是什么?
+
+My answer:
+
+```
+I think computers are more sold online, because the price is cheaper online
+and I can easily get more information about the computer.
+
+I bought my computer over internet on may this year, because the old one is broken, so I must buy a new one.
+
+I think the most important thing is the quality.
+firstly, a lot of people can’t work efficiently without computer, if the computer often broken, we will waste much time.
+secondly, out important information is store in the computer, if the computer often broken, we will waste many information.
+(you know, time and information are so expensive, so I think quality is the most important thing of computer.)
+```
+
+### 4.4	part4（8~10）(15+15+30) 题目是听的
+1. 会议什么时候举行，on Mondy  1月17日；地点是**Hall；
+2. xxx是6点开始吗？不是，5：30分；
+2. 对geogre bryant（布莱恩特）感兴趣：有两场report，1 在6：15 ,2在…
+
+My answer:
+
+```
+The meeting is held in XXX Hall on Monday January the 17th.  Please remember that.
+
+The XXX meeting opens from 5:30 am to 6:30 am on Monday. Please remember that.
+
+There are two sessions related to Geogre Bryant. 
+First, there is a session about XX security from 6:15 am to 11 pm. the session is given by xxx
+Next, There is a session about YY security from 3 pm to 4 pm, the session is given by xxx
+Please keep that in mind.
+```
+
+
+### 4.5	part5（11）(60)
+大题：如果你接到一个工作offer，你会和家里人family members 商量吗？ 和家人讨论的优点是什么？
+
+My answer(需要重新梳理):
+
+```
+According my personal experience, If I have a new offer, I will discuss with my family members, 
+There are some advantages as follows:
+Firstly: My parents have 20 years of working experience, t
+hey can help me recognize the weaknesses and the advantages of my new job.
+Secondly: Changing jobs is a big deal for a family. 
+I must make good communications with them, they will feel better, we must respect each other.
+Thirdly: With family member's support we will work efficiently.
+So I think we must discuss with my family members when I have a new offer.
+```
+
+
+
+## 2.   真题2(工作中哪个能力最重要)
+2021. 8/15
+### 2.1 part1（1~2）(45+45)
+### 2.2 part2（3 ~ 4）(30+30) 6~8个句子
+
+人很多的饭店，或坐着吃东西，或在交流。近景是三个人，一个人在吃薯条，另外两人在看菜单点餐，点餐的两个人中，其中一个男生拿着水（考过）
+
+My answer:
+
+```
+The picture shows there are many people in a restaurant. 
+Most of them are eating food and talking. 
+In the middle of the picture, there are 3 people sitting around a small table. One man is eating chips. 
+The other 2 are looking at a menu and ordering food. One of them is carrying a cup of water.
+From the picture we can see, the restaurant business is quite good, the food may be tasty.
+```
+
+一个仓库里面，右边两个女的在衣柜那里准备换衣服，一个女（左边）的是长头发、棕色裙子，另一个女的（右边）是白毛衣。左边有一个穿着蓝色上衣的胖男人的背影，在往远处走，仓库里面很多货架货物. 
+
+My answer:
+
+```
+The picture is taken in warehouse. There are two women and one man in this picture. 
+On the left side, there are two women changing clothes near to the cabinet in the dressing room. 
+One of them has long hair and wearing a brown dress, other one is wearing a white sweater. 
+One the right side, there is a fat man wearing the blue shirt is walking in the distance. 
+From the picture we can see, there are so many goods on shelves was piled in the warehouse.
+```
+
+### 2.3 part3（5~7）(15+15+30)
+什么时候离开hometown去城市的？你在hometown待了多长的时间
+很多人离开家乡的原因是什么
+一生在家乡长大的人的优势是啥
+
+My answer:
+
+```
+I lived in my hometown for 18 years (when I was born in a small village in Anhui Province). 
+I left my hometown about ten years ago when I needed to go to college.
+
+Many people leave their hometown for many reasons, such as work/study/business. 
+I left my hometown for the education, there is no university in my hometown.
+
+People who have always lived in their hometown will easy to work and study.
+Because they live with their families, old friends and relatives who will help them when they meet difficulties in life.
+that feels very good and that will save many money and time.
+They know everything and everyone in their hometown very well.
+So it's easier to succeed if they always lived in their hometown.
+
+```
+
+### 2.4 part4（8~10）(15+15+30) 题目是听的
+会议 议程
+图表是在印度一个饭店有个数据库会议，问题：
+1,会议在哪一天什么时间举行，会议的日期，开始时间，
+2,地址是什么hotel， 对吗。
+3.关于安全的会议都有哪几场，图标中有2场，上午和下午都一场
+
+My answer:
+
+```
+Hello Sir, The XX Database Meeting will be held on May The 20th The meeting opens from 9 am to 5 am on Monday.
+
+No, I am sorry, I am afraid that XX Database Meeting is Held in The Crown Hotel. Please remember that.
+
+Yes, according to information, there are 2 sessions about Database security. 
+First, there is session about XX security from 10am to 11 pm. 
+Next, there is a session about YY security from 3 pm to 4 pm. 
+Please keep that in mind.
+```
+
+### 2.5 part5（11）(60)
+你认为在就职一份新工作时   work hard willingness 还是colleague communicate well更重要？-努力工作的意识和跟人相处、协作能力哪个更重要
+
+My answer1:
+
+```
+I think colleague communicate well is the most important. there are some reasons as follows:
+Firstly, good communication enable us to express our views clearly, and share out views with our colleagues. 
+Secondly, good communication enable us to think about other one's views, it very important for our work. 
+Finally, good communication can make team members feel better, which can makes our work efficiently.
+The above reasons are why I think colleague communicate well is the most important.
+```
